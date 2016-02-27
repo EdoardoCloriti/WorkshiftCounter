bf475a5aa4898314146be1f90e35dc7638e31db9
Edoardo <edoardo.cloriti@gmail.com>
2016-02-27 19:41:44 +0100

    WSC-10 Separazione Activity Selezione Giorno e Display turno

--- app/src/main/java/com/orion/workshiftmanager/display/DisplayWorkShift.java
+++ app/src/main/java/com/orion/workshiftmanager/display/DisplayWorkShift.java
@@ -1,13 +1,13 @@
 package com.orion.workshiftmanager.display;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.orion.workshiftmanager.R;
@@ -33,14 +33,15 @@ public class DisplayWorkShift extends Activity {
         calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+                Intent i = new Intent(getApplicationContext(), DisplayTurn.class);
                 GregorianCalendar day = new GregorianCalendar(year, month, dayOfMonth);
-
                 SimpleDateFormat sdf = new SimpleDateFormat(Turn.PATTERN);
                 String selectedDay = new String(sdf.format(day.getTime()));
                 Toast.makeText(getApplicationContext(), selectedDay, Toast.LENGTH_SHORT).show();
                 AccessToDB db = new AccessToDB();
                 Turn turn = db.getTurnBySelectedDay(selectedDay, getApplicationContext());
-                visualTurn(turn);
+                Turn.intentByTurn(i, turn);
+                startActivity(i);
             }
 
         });
@@ -83,60 +84,4 @@ public class DisplayWorkShift extends Activity {
         calendar.setSelectedDateVerticalBar(R.color.darkgreen);
     }
 
-    private void visualTurn(Turn turn) {
-        setContentView(R.layout.activity_display);
-
-        TextView title = (TextView) findViewById(R.id.title);
-
-        TextView inizioMattina = (TextView) findViewById(R.id.iniziovalue);
-        TextView fineMattina = (TextView) findViewById(R.id.finevalue);
-
-        TextView inizioPomeriggio = (TextView) findViewById(R.id.iniziopomeriggio);
-        TextView finePomeriggio = (TextView) findViewById(R.id.finepomeriggio);
-
-        TextView importante = (TextView) findViewById(R.id.importante);
-        TextView overValue = (TextView) findViewById(R.id.overvalue);
-        TextView orevalue = (TextView) findViewById(R.id.orevalue);
-
-        Button back = (Button) findViewById(R.id.back);
-
-        title.setText(turn.getDatariferimento());
-
-        if (turn.getInizioMattina() != null && turn.getFineMattina() != null && !isNull(turn.getInizioMattina(), turn.getFineMattina())) {
-            inizioMattina.setText(turn.getInizioMattina());
-            fineMattina.setText(turn.getFineMattina());
-        } else {
-            inizioMattina.setText(R.string.riposo);
-            fineMattina.setText(R.string.riposo);
-        }
-
-        if (turn.getInizioPomeriggio() != null && turn.getFinePomeriggio() != null && !isNull(turn.getInizioPomeriggio(), turn.getFinePomeriggio())) {
-            inizioPomeriggio.setText(turn.getInizioPomeriggio());
-            finePomeriggio.setText(turn.getFinePomeriggio());
-        } else {
-            inizioPomeriggio.setText(R.string.riposo);
-            finePomeriggio.setText(R.string.riposo);
-        }
-
-        orevalue.setText(Double.toString(turn.getHour()));
-        overValue.setText(Double.toString(turn.getOvertime()));
-
-        if (turn.getIsImportante()) {
-            importante.setTextColor(getResources().getColor(R.color.Red));
-        } else {
-            importante.setTextColor(getResources().getColor(R.color.transparent));
-        }
-        back.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                finish();
-
-            }
-        });
-    }
-
-    private boolean isNull(String value1, String value2) {
-        return "null:null".equalsIgnoreCase(value1) && "null:null".equalsIgnoreCase(value2);
-    }
 }
